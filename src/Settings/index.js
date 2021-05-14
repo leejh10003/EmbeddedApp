6c52ff883b3431ee8a173ff9471d3a3f3f43e25f
Lee, JunHyuk <leejh10003@gmail.com>
2021-05-14 23:28:08 +0900

    Add module import

--- src/Settings/index.js
+++ src/Settings/index.js
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import { Text, View, SafeAreaView } from 'react-native';
 import { createStackNavigator } from '@react-navigation/stack';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
 import DetailsScreen from '../Details';
 import { Button, Divider, Layout, TopNavigation } from '@ui-kitten/components';
 
